e813fc392d8dd55a61cd0e2ff1d7a087cac2b7a4
Damon Salvatore <61008596@qq.com>
2017-07-31 17:07:24 +0800

    edit

--- production/test/test.js
+++ production/test/test.js
@@ -1,9 +1,1 @@
-'use strict';
-
-var moment = require('moment');
-var now = moment();
-var daysNum = Math.floor((now - moment('2017-04-05 23:55:00')) / (24 * 60 * 60 * 1000));
-var addDaysNum = Math.floor(((now - moment('2017-04-05 23:55:00')) / (24 * 60 * 60 * 1000) % 1 * 60 * 24 - 550) / 5);
-addDaysNum = addDaysNum >= 0 ? addDaysNum : 0;
-var currentPeriodNum = 179 * daysNum + 816323 + addDaysNum;
-console.log(currentPeriodNum);
+"use strict";
